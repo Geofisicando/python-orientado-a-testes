98f9a9d2ef5a08ce4d304a7f3de65f1467127f76
Dirack <rodolfo_profissional@hotmail.com>
2024-11-23 06:45:34 -0300

    Estudo sobre raise exception

---
+++ exemplos/pytest_exemplos/raise/test_raise.py
@@ -0,0 +1,14 @@
+import pytest
+
+def soma(x,y):
+	if type(x) not in [int] or type(y) not in [int]:
+		raise TypeError("x e y precisam ser do tipo int!")
+	return x+y
+
+def test_typeerror_raise():
+	with pytest.raises(TypeError):
+		soma('1',2)
+	with pytest.raises(TypeError):
+		soma(1,'2')
+	with pytest.raises(TypeError):
+		soma('1','1')
